e4c1eb949ba1843e1fcb1c7b0cd1f6f3000d752d
Siddharth Shah <siddharthshah23@outlook.com>
2020-07-07 17:08:33 -0400

    Added navbar

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+const Navbar = ({ title, icon }) => {
+  return (
+    <div className="navbar bg-primary">
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+    </div>
+  );
+};
+Navbar.defaultProps = {
+  title: "Conatct Keeper",
+  icon: "fas fa-id-card-alt",
+};
+export default Navbar;
