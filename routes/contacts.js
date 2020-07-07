9bef2888b3105f11e99d24666a042d8d518e742a
Siddharth Shah <siddharthshah23@outlook.com>
2020-07-07 16:12:18 -0400

    Back-end connectivity completed

--- routes/contacts.js
+++ routes/contacts.js
@@ -34,7 +34,33 @@ router.put("/:id", (req, res) => res.send("Update contacts"));
 // DESC : ADD contact
 // @access : Private
 
-router.post("/", (req, res) => res.send("Add contacts"));
+router.post(
+  "/",
+  [auth, [check("name", "Name is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    const { name, email, phone, type } = req.body;
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id,
+      });
+
+      const contact = await newContact.save();
+
+      res.json(contact);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 // @route : PUT api:id/contacts
 // DESC : Delete contact
