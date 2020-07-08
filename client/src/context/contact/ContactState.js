607aaca6705f3ca314a7ba3761397dbfad647fec
Siddharth Shah <siddharthshah23@outlook.com>
2020-07-08 13:53:28 -0400

    Adding context

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -10,3 +10,57 @@ import {
   FILTER_CONTACT,
   CLEAR_FILTER,
 } from "../types";
+
+const ContactState = (props) => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: "Harin Shah",
+        email: "harinshah45@yahoo.com",
+        phone: "6478699079",
+        type: "personal",
+      },
+
+      {
+        id: 2,
+        name: "Siddharth Shah",
+        email: "sid35@yahoo.com",
+        phone: "6473579079",
+        type: "Bussiness",
+      },
+
+      {
+        id: 1,
+        name: "Alex smith",
+        email: "alexsmith@yahoo.com",
+        phone: "6478645829",
+        type: "personal",
+      },
+    ],
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  //Add contact
+
+  //Delte contact
+
+  //Update contact
+
+  // Filter contact
+
+  // clear filter
+
+  // Set current
+
+  // clear current
+
+  return (
+    <ContactContext.Provider value={{ contacts: state.contacts }}>
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
