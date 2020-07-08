2517676d2e499676705f8ed2f5323460b0f342d4
Siddharth Shah <siddharthshah23@outlook.com>
2020-07-08 13:25:49 -0400

    Adding context

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,12 @@
+import React, { useReducer } from "react";
+import contactContext from "./contactContext";
+import contactReducer from "./contactReducer";
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACT,
+  CLEAR_FILTER,
+} from "../types";
